342f00e87237692433efa6f4fd8682a1b6e666ae
anny_more <anxxxen@ya.ru>
2021-08-19 13:48:44 +0300

    add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const str = Math.abs(n).toString();
+    let result = '';
+    for (let i = 0; i < str.length; i += 1) {
+         result = `${str[i]}${result}`;
+    }
+    return Number(result);
 }
